a052663661d94149358c59631a8bb29cf674a723
maridabrins <maridabrins@hotmail.com>
2024-11-15 22:13:09 -0300

    finishing CRUD

---
+++ MD-classManager.backend/src/main/java/com/mariana/aulas/controller/AulaController.java
@@ -0,0 +1,53 @@
+package com.mariana.aulas.controller;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mariana.aulas.model.AulaModel;
+import com.mariana.aulas.service.AulaService;
+
+@RequestMapping
+@RestController
+public class AulaController {
+	
+	@Autowired
+	private AulaService service;
+	
+	@GetMapping("/aulas")
+	public List<AulaModel> getAulas(){
+		return service.getAula();
+	}
+	
+	@GetMapping("/aulas/{id}")
+	public AulaModel getAula(@PathVariable UUID id) {
+		return service.getAulaById(id);
+	}
+	  
+    @PostMapping("/aulas/criar-nova")
+    public AulaModel createAula(@RequestBody AulaModel aula) {
+    	return service.saveAula(aula);
+    }
+    
+   @PutMapping("/aulas/editar/{id}")
+   public AulaModel updateAula(@PathVariable UUID id, @RequestBody AulaModel aula) {
+	   return service.updateAula(id, aula);
+   }
+	
+   @DeleteMapping("/aulas/deletar/{id}")
+   public void deleteAula(@PathVariable UUID id) {
+	 service.deleteAula(id);
+   }
+	
+	
+
+}
